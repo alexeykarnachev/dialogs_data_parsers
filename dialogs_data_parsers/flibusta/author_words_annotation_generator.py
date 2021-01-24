e21fbc1b2bd9a6eb6c86d22ecf11febf9d327aaa
Alexey Karnachev <alekseykarnachev@gmail.com>
2021-01-24 18:15:56 +0000

    Add flibusta dialog aumentator / annatator.

---
+++ dialogs_data_parsers/flibusta/author_words_annotation_generator.py
@@ -0,0 +1,82 @@
+from itertools import chain, cycle
+import json
+from pathlib import Path
+import random
+import re
+
+from dialogs_data_parsers.flibusta.dialogs_parser import DIALOG_SEPARATORS
+
+_PERSON_FLAG = 0
+_AUTHOR_FLAG = 1
+_PUNCT_FLAG = 2
+_DASH_FLAG = 3
+_SPLIT_FLAGS = (_PERSON_FLAG, _PUNCT_FLAG, _DASH_FLAG, _AUTHOR_FLAG, _PUNCT_FLAG, _DASH_FLAG)
+
+_DIALOGS_SEPARATORS_SET = set(DIALOG_SEPARATORS)
+_AUTHOR_WORDS_SEPARATOR_PATTERN = re.compile(f'([.,!?:;]+)(\s?[{DIALOG_SEPARATORS}])')
+_AUGMENT_PUNCT_CHOICES = list(set(chain(*[[symbol * i for i in range(0, 4)] for symbol in '.,!?:; '])))
+_AUGMENT_DASH_CHOICES = list(
+    set(chain(*[[' ' * i + symbol for i in range(0, 4)] for symbol in list(_DIALOGS_SEPARATORS_SET) + [' ']])))
+
+
+class FlibustaAuthorWordsAnnotationGenerator:
+    def __init__(self, raw_dialogs_file_path, out_file_path, n_samples=1e6, augment_p=0.3):
+        self._raw_dialogs_file_path = raw_dialogs_file_path
+        self._augment_p = augment_p
+        self._out_file_path = Path(out_file_path)
+        self._n_samples = int(n_samples)
+
+    def run(self):
+        utterances = self._iterate_on_utterances()
+        n_samples_done = 0
+
+        self._out_file_path.parent.mkdir(exist_ok=True, parents=True)
+
+        with open(self._out_file_path, 'w') as out_file:
+            for utterance in utterances:
+                augmented_split_utterance_and_flags = self._generate_augmented_split_utterance_and_flags(utterance)
+                if len(augmented_split_utterance_and_flags) > 1:
+                    payload = json.dumps(augmented_split_utterance_and_flags, ensure_ascii=False)
+                    out_file.write(payload)
+                    out_file.write('\n')
+                    n_samples_done += 1
+
+                    if n_samples_done == self._n_samples:
+                        break
+
+                    if n_samples_done % 10000 == 0:
+                        print(f'Samples: {n_samples_done}/{self._n_samples}')
+
+    def _iterate_on_utterances(self):
+        with open(self._raw_dialogs_file_path) as file:
+            for line in file:
+                dialog = json.loads(line)
+                for utterance in dialog:
+                    yield utterance
+
+    def _generate_augmented_split_utterance_and_flags(self, utterance):
+        split_utterance = _AUTHOR_WORDS_SEPARATOR_PATTERN.split(utterance)
+
+        augmented_split_utterance = []
+        augmented_split_utterance_flags = []
+
+        for sub_utterance, flag in zip(split_utterance, cycle(_SPLIT_FLAGS)):
+            if flag != _PUNCT_FLAG and flag != _DASH_FLAG:
+                augmented_split_utterance.append(sub_utterance)
+                augmented_split_utterance_flags.append(flag)
+            else:
+                if random.random() <= self._augment_p:
+                    choices = _AUGMENT_PUNCT_CHOICES if flag == _PUNCT_FLAG else _AUGMENT_DASH_CHOICES
+                    sub_utterance = random.choice(choices)
+
+                augmented_split_utterance[-1] += sub_utterance
+
+        augmented_split_utterance_and_flags = list(zip(augmented_split_utterance, augmented_split_utterance_flags))
+
+        return augmented_split_utterance_and_flags
+
+
+if __name__ == '__main__':
+    gen = FlibustaAuthorWordsAnnotationGenerator(
+        '/data/dialogs/flibusta/raw_dialogs.jsonl', out_file_path='/data/dialogs/flibusta/annotated_dialogs.jsonl')
+    gen.run()
--- dialogs_data_parsers/flibusta/dialogs_parser.py
+++ dialogs_data_parsers/flibusta/dialogs_parser.py
@@ -1,61 +1,75 @@
 import json
 import logging
+import multiprocessing
+from pathlib import Path
 import re
 import unicodedata
-
-from itertools import chain, islice
-from pathlib import Path
 from zipfile import BadZipFile, ZipFile
 
 import bs4
-
 from more_itertools import chunked
-from tqdm import tqdm
 
 _logger = logging.getLogger(__name__)
+logging.getLogger("filelock").setLevel(logging.WARNING)
+
+DIALOG_SEPARATORS = '-‐‑‒–—―₋−⸺⸻﹘﹣－'
 
 
 class FlibustaDialogsParser:
     _MIN_N_UTTERANCES = 2
     _ARCHIVE_PATTERN = re.compile(r'.+fb2-.+\.zip')
-    _DIALOG_SEPARATORS = '-‐‑‒–—―₋−⸺⸻﹘﹣－'
-    _AUTHOR_WORDS_SEPARATOR_PATTERN = re.compile(f'([.,!?]) +[{_DIALOG_SEPARATORS}]')
+
+    _DIALOGS_CHUNK_WRITE_SIZE = 1000
 
     def __init__(self, flibusta_archives_dir, out_file_path):
         self._flibusta_archives_dir = flibusta_archives_dir
-        self._out_file_path = out_file_path
+        self._out_file_path = Path(out_file_path)
+        self._out_file_path.parent.mkdir(exist_ok=True, parents=True)
+        if self._out_file_path.is_file():
+            self._out_file_path.unlink()
+
+        manager = multiprocessing.Manager()
+        self._archives_counter = manager.Value('i', 0)
+        self._dialogs_counter = manager.Value('i', 0)
+        self._out_file_lock = manager.Lock()
+
+    def run(self):
+        archive_paths = list(self._iterate_on_archive_paths())
+
+        with multiprocessing.Pool() as pool:
+            pool.map(self._parse_archive, archive_paths)
 
     def _iterate_on_archive_paths(self):
         for path in Path(self._flibusta_archives_dir).iterdir():
             if self._ARCHIVE_PATTERN.match(path.name):
                 yield path
 
-    def _iterate_on_raw_fb2_texts(self):
-        archive_paths = tqdm(self._iterate_on_archive_paths(), desc='Archives')
+    def _parse_archive(self, archive_path):
+        dialogs = self._iterate_on_dialogs(archive_path)
 
-        for archive_path in archive_paths:
-            try:
-                with ZipFile(archive_path, 'r') as zip_file:
-                    for file_name in zip_file.namelist():
-                        data = zip_file.read(file_name)
-                        yield data
-            except BadZipFile:
-                _logger.warning(f'Bad zip file: {archive_path}')
+        for dialogs_chunk in chunked(dialogs, n=self._DIALOGS_CHUNK_WRITE_SIZE):
+            payloads = []
+            for dialog in dialogs_chunk:
+                payload = json.dumps(dialog, ensure_ascii=False)
+                payloads.append(payload)
 
-    def _iterate_on_book_texts(self):
-        raw_fb2_texts = tqdm(self._iterate_on_raw_fb2_texts(), desc='Books')
+            chunk_payload = '\n'.join(payloads)
 
-        for raw_fb2_text in raw_fb2_texts:
-            book_soup = bs4.BeautifulSoup(raw_fb2_text, features="html.parser")
-            lang_tag = book_soup.find('lang')
+            self._out_file_lock.acquire()
+            with open(self._out_file_path, 'a') as out_file:
+                out_file.write(chunk_payload)
+                out_file.write('\n')
+                out_file.flush()
+            self._out_file_lock.release()
 
-            if lang_tag and lang_tag.text.lower().strip() == 'ru':
-                book_text = book_soup.text
-                yield book_text
+            self._dialogs_counter.value += len(dialogs_chunk)
+            _logger.info(f'Archives: {self._archives_counter.value}, Dialogs: {self._dialogs_counter.value}')
 
-    def _iterate_on_book_dialogs(self):
-        book_texts = self._iterate_on_book_texts()
-        dialog_separators_set = set(self._DIALOG_SEPARATORS)
+        self._archives_counter.value += 1
+
+    def _iterate_on_dialogs(self, archive_path):
+        book_texts = self._iterate_on_book_texts(archive_path)
+        dialog_separators_set = set(DIALOG_SEPARATORS)
 
         for book_text in book_texts:
             book_text_lines = re.split('\n+', book_text)
@@ -66,10 +80,8 @@ class FlibustaDialogsParser:
 
                 if len(line) > 2 and line[0] in dialog_separators_set:
                     line = unicodedata.normalize("NFKC", line)
-                    split_line = self._AUTHOR_WORDS_SEPARATOR_PATTERN.split(line)
-                    utterance = ''.join(chain(*islice(chunked(split_line, 2), None, None, 2)))
-                    utterance = re.sub(r'^\W+', '', utterance)
-                    dialog.append(utterance)
+                    line = re.sub(r'^\W+', '', line)
+                    dialog.append(line)
                 else:
                     if len(dialog) >= self._MIN_N_UTTERANCES:
                         yield dialog
@@ -79,11 +91,16 @@ class FlibustaDialogsParser:
             if len(dialog) >= self._MIN_N_UTTERANCES:
                 yield dialog
 
-    def run(self):
-        dialogs = tqdm(self._iterate_on_book_dialogs(), desc='Dialogs')
-        out_file = open(self._out_file_path, 'w')
+    def _iterate_on_book_texts(self, archive_path):
+        try:
+            with ZipFile(archive_path, 'r') as zip_file:
+                for file_name in zip_file.namelist():
+                    raw_fb2_text = zip_file.read(file_name)
+                    book_soup = bs4.BeautifulSoup(raw_fb2_text, features="html.parser")
+                    lang_tag = book_soup.find('lang')
 
-        for dialog in dialogs:
-            payload = json.dumps(dialog, ensure_ascii=False)
-            out_file.write(payload)
-            out_file.write('\n')
+                    if lang_tag and lang_tag.text.lower().strip() == 'ru':
+                        book_text = book_soup.text
+                        yield book_text
+        except BadZipFile:
+            _logger.warning(f'Bad zip file: {archive_path}')
